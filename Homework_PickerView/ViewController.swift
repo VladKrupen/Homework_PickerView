6aa214e72506d6e699c20e905a74344e5070567a
Vlad Krupen <air7360@gmail.com>
2023-12-16 18:36:36 +0300

    added PHPickerViewController

--- Homework_PickerView/ViewController.swift
+++ Homework_PickerView/ViewController.swift
@@ -6,14 +6,63 @@
 //
 
 import UIKit
+import PhotosUI
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, PHPickerViewControllerDelegate {
+   
+    
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    @IBOutlet weak var pickerView: UIPickerView!
+    
+    lazy var picker: PHPickerViewController = {
+        var configuration = PHPickerConfiguration(photoLibrary: .shared())
+        configuration.selectionLimit = 10
+        let picker = PHPickerViewController(configuration: configuration)
+        picker.delegate = self
+        
+        return picker
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        imageView.contentMode = .scaleAspectFill
+
     }
 
-
+    @IBAction func buttonAddPhotoTapped(_ sender: UIButton) {
+        present(picker, animated: true)
+    }
+    
+    // MARK: - PHPickerViewControllerDelegate
+    
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        
+        let itemProviders = results.map { $0.itemProvider }
+        
+        for item in itemProviders {
+            item.loadObject(ofClass: UIImage.self) { image, error in
+                let image = image as? UIImage
+                
+                DispatchQueue.main.async {
+                    self.imageView.image = image
+                }
+            }
+        }
+        
+        picker.dismiss(animated: true)
+        
+        // MARK: - Button cancel
+        if results.isEmpty {
+            picker.dismiss(animated: true)
+        }
+        
+    }
+    
 }
 
