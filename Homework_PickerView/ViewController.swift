4270cdcd692debf2da66e8abe5eca3f01b7ad293
Vlad Krupen <air7360@gmail.com>
2023-12-16 23:11:52 +0300

    added UIPickerView

--- Homework_PickerView/ViewController.swift
+++ Homework_PickerView/ViewController.swift
@@ -8,10 +8,8 @@
 import UIKit
 import PhotosUI
 
-class ViewController: UIViewController, PHPickerViewControllerDelegate {
+class ViewController: UIViewController, PHPickerViewControllerDelegate, UIPickerViewDelegate, UIPickerViewDataSource {
    
-    
-    
     
     @IBOutlet weak var imageView: UIImageView!
     
@@ -27,12 +25,18 @@ class ViewController: UIViewController, PHPickerViewControllerDelegate {
         return picker
     }()
     
+    var selectedImages: [UIImage] = []
     
+   
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         imageView.contentMode = .scaleAspectFill
-
+        
+        pickerView.delegate = self
+        pickerView.dataSource = self
+        
     }
 
     @IBAction func buttonAddPhotoTapped(_ sender: UIButton) {
@@ -44,13 +48,15 @@ class ViewController: UIViewController, PHPickerViewControllerDelegate {
     func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
         
         let itemProviders = results.map { $0.itemProvider }
-        
+    
         for item in itemProviders {
             item.loadObject(ofClass: UIImage.self) { image, error in
-                let image = image as? UIImage
                 
                 DispatchQueue.main.async {
-                    self.imageView.image = image
+                    if let image = image as? UIImage {
+                        self.selectedImages.append(image)
+                        self.pickerView.reloadAllComponents()
+                    }
                 }
             }
         }
@@ -61,8 +67,43 @@ class ViewController: UIViewController, PHPickerViewControllerDelegate {
         if results.isEmpty {
             picker.dismiss(animated: true)
         }
+    }
+    
+    
+    // MARK: - UIPickerViewDataSource
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return selectedImages.count
+    }
+    
+    // MARK: - UIPickerViewDelegate
+    
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        return 120
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        
+        let pickerImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        
+        pickerImageView.contentMode = .scaleAspectFill
+        pickerImageView.clipsToBounds = true
+        pickerImageView.image = selectedImages[row]
+        pickerImageView.layer.cornerRadius = 50
+
+        return pickerImageView
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        imageView.image = selectedImages[row]
         
     }
+    
     
 }
 
